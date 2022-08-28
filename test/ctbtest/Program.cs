e13f474eeb99ee020a8a17b998d50871841e50f8
jruben <jr@anon.com>
2022-08-28 17:48:20 -0500

    SdoReadOK_usingPipies

--- test/ctbtest/Program.cs
+++ test/ctbtest/Program.cs
@@ -1,15 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics;
+
 statusReadLoop.Init();
 commandWriteLoop.Init();
 
-
+int cycleCount = 0;
 while (true)
 {
     //Console.WriteLine("SENDING REQUEST");
+    cycleCount++;
+    var ms = new Stopwatch();
+    ms.Start();
     var result = await EthercatNetwork.SdoRead(1, 0x1A00, 01);
-    Console.WriteLine(Convert.ToHexString (result.PayloadResponseBytes!));
-    Console.ReadLine();
+    ms.Stop();
+    Console.WriteLine($"{cycleCount.ToString().PadLeft(5)} (Hex): {Convert.ToHexString (result.PayloadResponseBytes!).PadLeft(10) } in {ms.Elapsed.TotalMilliseconds.ToString("# ##0").PadLeft(6)} ms ");
+    await Task.Delay(1000);
+
+
 }
 
 
